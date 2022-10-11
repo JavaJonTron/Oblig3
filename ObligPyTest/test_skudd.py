771bbf7ea9c587adf74f1919960cb52a3bf42770
JavaJonTron <113911659+JavaJonTron@users.noreply.github.com>
2022-10-11 13:35:55 +0200

    first commit
    Committing project to repository

---
+++ ObligPyTest/test_skudd.py
@@ -0,0 +1,18 @@
+import main
+
+def test_leapyear_divisible_by_4_but_not_by_100():
+    assert main.isLeapYear(2012) is True
+    assert main.isLeapYear(1964) is True
+
+def test_leapyear_divisible_by_400():
+    assert main.isLeapYear(1600) is True
+    assert main.isLeapYear(2800) is True
+
+def test_leapyear_not_divisible_by_4():
+    assert main.isLeapYear(2003) is False
+    assert main.isLeapYear(2005) is False
+
+
+def test_leapyear_divisible_by_100_but_not_by_400():
+    assert main.isLeapYear(1900) is False
+    assert main.isLeapYear(2100) is False
